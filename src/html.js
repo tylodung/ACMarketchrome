fc4a403f6400a7e659520b5b51facf165a52e9e0
tylodung <39002385+tylodung@users.noreply.github.com>
2019-03-16 12:48:18 +0700

    up

--- src/html.js
+++ src/html.js
@@ -68,7 +68,7 @@ export default class HTML extends Component {
 function buildGoogleAdSense() {
   const js = `
   (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: 'ca-pub-9432383680259907',
+    google_ad_client: 'ca-pub-3207311307713306',
     enable_page_level_ads: true
   });
   `;
